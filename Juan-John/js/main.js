e8c8af31280af8a65f31737c71327512497b3edd
Gazwahi <Igazwah@gmail.com>
2026-04-13 01:09:55 -0400

    updated contact page and added map

--- Juan-John/js/main.js
+++ Juan-John/js/main.js
@@ -4,3 +4,13 @@ const mobileMenu = document.getElementById("mobile-menu");
 menuBtn.addEventListener("click", function () {
   mobileMenu.classList.toggle("hidden");
 });
+document.querySelectorAll(".faq-question").forEach(btn => {
+  btn.addEventListener("click", () => {
+    const answer = btn.nextElementSibling;
+    answer.classList.toggle("active");
+  });
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  alert("Message sent!");
+});
